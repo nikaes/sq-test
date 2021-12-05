3454d334eb3ed9766e2b2df5f8c1800feda4dd85
Akin Aktemur <akinaktemur@yahoo.de>
2021-12-05 09:17:10 +0100

    test

---
+++ pytest.py
@@ -0,0 +1,15 @@
+def AA():
+    if(True):
+        return 1
+    if(False):
+        return 1
+        
+def BB():
+    if(True):
+        return 1
+    if(False):
+        return 1
+
+
+if __name__ == '__main__':
+    print(f"setting document {doc} in index {myindex} ")
